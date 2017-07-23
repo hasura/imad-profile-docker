de2560a075bdc71d470edb77a45fed45b954c516
Karthikeya Viswanath <viswanathkarthikeya@gmail.com>
2017-07-23 21:24:38 +0530

    Changed env var back to INDEX_HTML

--- app/scripts/getProfile.sh
+++ app/scripts/getProfile.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 
-if [[ -z "$indexHtml" ]]; then
+if [[ -z "$INDEX_HTML" ]]; then
     cp /usr/share/nginx/html/default.html /usr/share/nginx/html/index.html
 else
-    echo $indexHtml > "/usr/share/nginx/html/index.html"
+    echo $INDEX_HTML > "/usr/share/nginx/html/index.html"
 fi
 
 nginx -g 'daemon off;'
