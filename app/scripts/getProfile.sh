23b30a70a93504c0ca8a6d330528a0b72016ba47
Karthikeya Viswanath <viswanathkarthikeya@gmail.com>
2017-07-23 18:03:17 +0530

    Tested

--- app/scripts/getProfile.sh
+++ app/scripts/getProfile.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/bash
 
 if [[ -z "$INDEX_HTML" ]]; then
-    echo "<h1>Ollo!</h1>" > "/usr/share/nginx/html/index.html"
+    cp /usr/share/nginx/html/default.html /usr/share/nginx/html/index.html
 else
     echo $INDEX_HTML > "/usr/share/nginx/html/index.html"
 fi
+
+nginx -g 'daemon off;'
